53563c19d28b5f57f5f602f8f37e88886a97bcba
Danny <dannycorbett890@gmail.com>
2026-06-01 21:58:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# To-do List
+
+This is a todo list app written in c, mainly to learn the language
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("TODO List\n");
+    return 0;
+}
